240f1dd31bf4f66aceedf33909fef50a3ac1ae4a
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-05 20:17:28 +0900

    権限管理の実装

--- practice/iam_role/main.tf
+++ practice/iam_role/main.tf
@@ -7,6 +7,10 @@ resource "aws_iam_role" "default" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+/*
+  ポリシードキュメントで「実行可能なアクション」や「操作可能なリソース」を指定する
+  identifiers で関連づけることのできるサービスを指定できる(信頼ポリシー)
+*/
 data "aws_iam_policy_document" "assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -18,11 +22,18 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+/*
+  ポリシードキュメントを保持するリソースが IAMポリシー
+  ポリシー名とポリシードキュメントを設定する
+*/
 resource "aws_iam_policy" "default" {
   name = var.name
   policy = var.policy
 }
 
+/*
+  IAM ロールに IAM ポリシーを関連づける
+*/
 resource "aws_iam_role_policy_attachment" "default" {
   role = aws_iam_role.default.name
   policy_arn = aws_iam_policy.default.arn
