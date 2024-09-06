b99d18e0f7fbf9062e8aba8b4e843f3b99349357
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-06 23:02:25 +0900

    パブリックバケットの作成

--- practice/s3.tf
+++ practice/s3.tf
@@ -36,3 +36,23 @@ resource "aws_s3_bucket_public_access_block" "private" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+}
+
+resource "aws_s3_bucket_acl" "public" {
+  bucket = aws_s3_bucket.public.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_cors_configuration" "public" {
+  bucket = aws_s3_bucket.public.id
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
+}
