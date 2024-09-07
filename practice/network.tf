afb49ba21577bdf74fdfe9f674af603ab22817d6
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-07 15:44:35 +0900

    NATゲートウェイの定義

--- practice/network.tf
+++ practice/network.tf
@@ -58,3 +58,9 @@ resource "aws_eip" "nat_gateway" {
   domain = "vpc"
   depends_on = [aws_internet_gateway.example]
 }
+
+resource "aws_nat_gateway" "example" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id = aws_subnet.public.id
+  depends_on = [aws_internet_gateway.example]
+}
