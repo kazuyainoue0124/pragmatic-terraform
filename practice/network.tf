08b5c83cfb64d746e099eb1b4e5d045494273777
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-07 15:52:17 +0900

    パブリックサブネットとルートテーブルの関連付けをマルチAZ化

--- practice/network.tf
+++ practice/network.tf
@@ -37,8 +37,13 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
+resource "aws_route_table_association" "public_0" {
+  subnet_id = aws_subnet.public_0.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_1" {
+  subnet_id = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
 }
 
