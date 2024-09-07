6499db552bf6a952543077489c33e018c5898e91
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-07 11:25:33 +0900

    ログバケットの作成

--- practice/s3.tf
+++ practice/s3.tf
@@ -56,3 +56,39 @@ resource "aws_s3_bucket_cors_configuration" "public" {
     max_age_seconds = 3000
   }
 }
+
+resource "aws_s3_bucket" "alb_log" {
+  bucket = "alb-log-pragmatic-terraform"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+
+  rule {
+    id     = "log_expiration"
+    status = "Enabled"
+
+    expiration {
+      days = 180
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
+
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
+
+    principals {
+      type = "AWS"
+      # AWS アカウント ID（書籍どおりの番号なので実際とは異なる）
+      identifiers = ["582318560864"]
+    }
+  }
+}
