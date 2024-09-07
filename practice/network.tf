077af3e388fa173a93e92e2af12643efbfa08655
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-07 16:27:29 +0900

    セキュリティグループのアウトバウンドルールを作成

--- practice/network.tf
+++ practice/network.tf
@@ -130,3 +130,12 @@ resource "aws_security_group_rule" "ingress_example" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.example.id
 }
+
+resource "aws_security_group_rule" "egress_example" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
