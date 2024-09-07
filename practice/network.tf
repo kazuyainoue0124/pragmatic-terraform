a11ab5b1d95a28286cded4a52437d1eefb6226f0
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-07 16:11:33 +0900

    セキュリティグループのインバウンドルールを作成

--- practice/network.tf
+++ practice/network.tf
@@ -121,3 +121,12 @@ resource "aws_security_group" "example" {
   name = "example"
   vpc_id = aws_vpc.example.id
 }
+
+resource "aws_security_group_rule" "ingress_example" {
+  type = "ingress"
+  from_port = "80"
+  to_port = "80"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
