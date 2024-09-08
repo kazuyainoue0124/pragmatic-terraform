4c3355e540d955b1e21b568cd8beaf0a3aa62510
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-08 16:31:15 +0900

    SSL証明書の定義

--- practice/lb.tf
+++ practice/lb.tf
@@ -79,6 +79,16 @@ resource "aws_route53_record" "example" {
   }
 }
 
+resource "aws_acm_certificate" "example" {
+  domain_name = aws_route53_record.example.name
+  subject_alternative_names = []
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
