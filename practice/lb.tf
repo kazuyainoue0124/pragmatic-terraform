4d614f6d7a29bd3aadade7688b08946158b21528
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-08 16:37:46 +0900

    SSL証明書の検証用レコードの定義

--- practice/lb.tf
+++ practice/lb.tf
@@ -89,6 +89,14 @@ resource "aws_acm_certificate" "example" {
   }
 }
 
+resource "aws_route53_record" "example_certificate" {
+  name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+  type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+  zone_id = data.aws_route53_zone.example.id
+  ttl = 60
+}
+
 output "alb_dns_name" {
   value = aws_lb.example.dns_name
 }
