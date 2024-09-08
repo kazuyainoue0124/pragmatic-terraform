5e20e890da2cbe85c3e2d44f93289c981e13477f
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-08 16:09:16 +0900

    DNSレコードの定義

--- practice/lb.tf
+++ practice/lb.tf
@@ -70,3 +70,19 @@ output "alb_dns_name" {
 data "aws_route53_zone" "example" {
   name = "pragmatic-terraform.com."
 }
+
+resource "aws_route53_record" "example" {
+  zone_id = data.aws_route53_zone.example.zone_id
+  name = data.aws_route53_zone.example.name
+  type = "A"
+
+  alias {
+    name = aws_lb.example.dns_name
+    zone_id = aws_lb.example.zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "domain_name" {
+  value = aws_route53_record.example.name
+}
