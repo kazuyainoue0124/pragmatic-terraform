0ba6b980060b320d6e2007541d2fbd3e8770e7be
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-08 17:52:33 +0900

    リクエストフォワード用のリスナールールを追加

--- practice/lb.tf
+++ practice/lb.tf
@@ -119,6 +119,22 @@ resource "aws_lb_target_group" "example" {
   depends_on = [aws_lb.example]
 }
 
+resource "aws_lb_listener_rule" "example" {
+  listener_arn = aws_lb_listener.https.arn
+  priority = 100
+
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/*"]
+    }
+  }
+}
+
 data "aws_route53_zone" "example" {
   name = "pragmatic-terraform.com."
 }
