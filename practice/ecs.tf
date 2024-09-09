69d4469c72abef97e680a321ee8ea5357626fbd8
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-09 20:50:33 +0900

    AmazonECSTaskExecutionRolePolicyの参照

--- practice/ecs.tf
+++ practice/ecs.tf
@@ -53,3 +53,7 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name = "/ecs/example"
   retention_in_days = 180
 }
+
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
