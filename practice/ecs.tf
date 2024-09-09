ad4dd09756718f35f015dd28226becb7b4588d96
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-09 20:58:33 +0900

    ECSタスク実行IAMロールの定義

--- practice/ecs.tf
+++ practice/ecs.tf
@@ -67,3 +67,10 @@ data "aws_iam_policy_document" "ecs_task_execution" {
     resources = ["*"]
   }
 }
+
+module "ecs_task_execution_role" {
+  source = "./iam_role"
+  name = "ecs-task-execution"
+  identifier = "ecs-tasks.amazonaws.com"
+  policy = data.aws_iam_policy_document.ecs_task_execution.json
+}
