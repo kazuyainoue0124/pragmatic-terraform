31b607c12078ba008c3d382da026a390bc675eb3
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-10 21:07:33 +0900

    データベースのパスワードのダミー定義

--- practice/ssm.tf
+++ practice/ssm.tf
@@ -11,3 +11,14 @@ resource "aws_ssm_parameter" "db_raw_password" {
   type = "SecureString"
   description = "データベースのパスワード"
 }
+
+resource "aws_ssm_parameter" "db_password" {
+  name = "/db/password"
+  value = "uninitialized"
+  type = "SecureString"
+  description = "データベースのパスワード"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
