4e4a3d3cbdfeba1e55d097119d59d5a8a59926f3
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-10 19:49:41 +0900

    エイリアスの定義

--- practice/kms.tf
+++ practice/kms.tf
@@ -4,3 +4,8 @@ resource "aws_kms_key" "example" {
   is_enabled = true
   deletion_window_in_days = 30
 }
+
+resource "aws_kms_alias" "example" {
+  name = "alias/example"
+  target_key_id = aws_kms_key.example.key_id
+}
