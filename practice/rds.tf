14eaa837d8cb8af7e206629e9fc45b3e603bfbc1
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-11 19:39:42 +0900

    DBインスタンスのセキュリティグループの定義

--- practice/rds.tf
+++ practice/rds.tf
@@ -59,3 +59,11 @@ resource "aws_db_instance" "example" {
     ignore_changes = [password]
   }
 }
+
+module "mysql_sg" {
+  source = "./security_group"
+  name = "mysql-sg"
+  vpc_id = aws_vpc.example.id
+  port = 3306
+  cidr_blocks = [aws_vpc.example.cidr_block]
+}
