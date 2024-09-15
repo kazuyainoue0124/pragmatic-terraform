db6b505bbf366a11ab4090e715f7ee73ce0d9ef4
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-15 22:12:20 +0900

    オペレーションサーバー用IAMロールの定義

--- practice/ec2.tf
+++ practice/ec2.tf
@@ -23,3 +23,10 @@ data "aws_iam_policy_document" "ec2_for_ssm" {
 data "aws_iam_policy" "ec2_for_ssm" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+module "ec2_for_ssm_role" {
+  source = "./iam_role"
+  name = "ec2_for_ssm"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.ec2_for_ssm.json
+}
