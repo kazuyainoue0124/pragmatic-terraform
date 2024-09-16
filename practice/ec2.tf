eea17274bce87e5fd959a78473addb444072a41a
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-16 10:33:17 +0900

    オペレーションサーバー用EC2インスタンスの定義

--- practice/ec2.tf
+++ practice/ec2.tf
@@ -35,3 +35,15 @@ resource "aws_iam_instance_profile" "ec2_for_ssm" {
   name = "ec2_for_ssm"
   role = module.ec2_for_ssm_role.iam_role_name
 }
+
+resource "aws_instance" "example_for_operation" {
+  ami = "ami-0c3fd0f5d33134a76"
+  instance_type = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id = aws_subnet.private_0.id
+  user_data = file("./user_data.sh")
+}
+
+output "operation_instance_id" {
+  value = aws_instance.example_for_operation.id
+}
