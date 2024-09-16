d3ae41414c51b510dddb8df97af305cd264fa70f
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-16 11:50:26 +0900

    Kinesis Data Firehose IAMロールの定義

--- practice/kinesis.tf
+++ practice/kinesis.tf
@@ -17,3 +17,10 @@ data "aws_iam_policy_document" "kinesis_data_firehose" {
     ]
   }
 }
+
+module "kinesis_data_firehose_role" {
+  source = "./iam_role"
+  name = "kinesis-data-firehose"
+  identifier = "firehose.amazonaws.com"
+  policy = data.aws_iam_policy_document.kinesis_data_firehose.json
+}
