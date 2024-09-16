c5a4f8738b8b3a3229aaf4a7bbee620b9ba46bd4
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-16 12:00:38 +0900

    CloudWatch Logs IAMロールの定義

--- practice/kinesis.tf
+++ practice/kinesis.tf
@@ -49,3 +49,10 @@ data "aws_iam_policy_document" "cloudwatch_logs" {
     resources = ["arn:aws:iam::*:role/cloudwatch-logs"]
   }
 }
+
+module "cloudwatch_logs_role" {
+  source = "./iam_role"
+  name = "cloudwatch-logs"
+  identifier = "logs.ap-northeast-1.amazonaws.com"
+  policy = data.aws_iam_policy_document.cloudwatch_logs.json
+}
