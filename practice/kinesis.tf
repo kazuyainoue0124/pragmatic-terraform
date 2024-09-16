27906fe389b345c497ea92010d856d6832d9ca28
kazuyainoue0124 <bauer24tbp@gmail.com>
2024-09-16 11:59:04 +0900

    CloudWatch Logs IAMロールのポリシードキュメントの定義

--- practice/kinesis.tf
+++ practice/kinesis.tf
@@ -35,3 +35,17 @@ resource "aws_kinesis_firehose_delivery_stream" "example" {
     prefix = "ecs-scheduled-tasks/example/"
   }
 }
+
+data "aws_iam_policy_document" "cloudwatch_logs" {
+  statement {
+    effect = "Allow"
+    actions = ["firehose:*"]
+    resources = ["arn:aws:firehose:ap-northeast-1:*:*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = ["iam:PassRole"]
+    resources = ["arn:aws:iam::*:role/cloudwatch-logs"]
+  }
+}
